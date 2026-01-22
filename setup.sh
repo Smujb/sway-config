2c13454a13ef00b3c76e4a8968d679efd802f744
JayB <smujamesb@gmail.com>
2026-01-22 08:20:23 +0000

    Added some defaults to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 
 # Remove existing local waybar config and create a symbolic link to the waybar config here
 rm -rf ~/.config/waybar && ln -s ~/.config/sway/waybar-config ~/.config/waybar
+
+# Set defaults
+xdg-mime default thunar.desktop inode/directory # File manager
+xdg-mime default org.xfce.mousepad.desktop text/plain # Text editor
